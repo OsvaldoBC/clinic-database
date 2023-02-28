662b3a6d01af6cefa3d4a0ef9d0d0d7fc43f054d
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-02-28 16:21:45 -0500

    create table treatments and table invoice_items

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -23,3 +23,23 @@ CREATE TABLE invoices(
   CONSTRAINT medical_history_id_fk FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
   PRIMARY KEY(id)
 );
+
+CREATE TABLE treatments(
+id INT GENERATED ALWAYS AS IDENTITY,
+type VARCHAR(250),
+name VARCHAR(250),
+PRIMARY KEY(id)
+);
+
+CREATE TABLE invoice_items(
+id INT GENERATED ALWAYS AS IDENTITY,
+unit_price DECIMAL,
+quantity INT,
+total_price DECIMAL,
+invoice_id INT,
+treatment_id INT,
+CONSTRAINT invoice_id_fk FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+CONSTRAINT treatment_id_fk FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+PRIMARY KEY(id)
+);
+
