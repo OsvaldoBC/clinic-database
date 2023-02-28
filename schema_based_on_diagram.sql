b27ec7bf99560a797d987ef8dcb37c57b7e451ff
Juank628 <jsanchez_zunino@yahoo.com>
2023-02-28 16:31:48 -0500

    index created for FK

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -51,3 +51,8 @@ CONSTRAINT histories_id_fk FOREIGN KEY (medical_histories_id) REFERENCES medical
 CONSTRAINT treatments_id_fk FOREIGN KEY (treatments_id) REFERENCES treatments(id),
 PRIMARY KEY(id)
 );
+
+CREATE INDEX medical_history_id_index ON invoices(medical_histories_id);
+CREATE INDEX patient_id_index ON medical_histories(patient_id);
+CREATE INDEX invoice_id_index ON invoice_items(invoice_id);
+CREATE INDEX treatment_id_index ON invoice_items(treatment_id);
