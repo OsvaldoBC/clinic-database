dc45af2bd72777e47bb931173b6897c291637687
Juank628 <jsanchez_zunino@yahoo.com>
2023-02-28 16:38:54 -0500

    index correction

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -52,7 +52,7 @@ CONSTRAINT treatments_id_fk FOREIGN KEY (treatments_id) REFERENCES treatments(id
 PRIMARY KEY(id)
 );
 
-CREATE INDEX medical_history_id_index ON invoices(medical_histories_id);
+CREATE INDEX medical_history_id_index ON invoices(medical_history_id);
 CREATE INDEX patient_id_index ON medical_histories(patient_id);
 CREATE INDEX invoice_id_index ON invoice_items(invoice_id);
 CREATE INDEX treatment_id_index ON invoice_items(treatment_id);
